3f433c99e27cf62c554b20293a5fcbc2e22f3c8c
Emma Kang <yuseon1216@gmail.com>
2021-03-28 23:27:29 -0400

    initial commit

---
+++ src/test/java/com/ekang/studyroom/StudyRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ekang.studyroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
