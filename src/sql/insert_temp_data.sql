3dcd03d1a37e86935333134bc4a5e92bac1aadd5
Emma Kang <yuseon1216@gmail.com>
2021-03-30 20:51:35 -0400

    add query file to insert temp data

---
+++ src/sql/insert_temp_data.sql
@@ -0,0 +1,38 @@
+insert into enum_contact(type_id, description) values (1, 'home phone');
+insert into enum_contact(type_id, description) values (2, 'mobile phone');
+insert into enum_contact(type_id, description) values (3, 'email');
+
+insert into rooms(room_id, room_name) values (1, 'Room A');
+insert into rooms(room_id, room_name) values (2, 'Room B');
+insert into rooms(room_id, room_name) values (3, 'Room C');
+
+insert into seats(seat_id, room_id) values (1, 1);
+insert into seats(seat_id, room_id) values (2, 1);
+insert into seats(seat_id, room_id) values (3, 1);
+insert into seats(seat_id, room_id) values (4, 1);
+insert into seats(seat_id, room_id) values (5, 1);
+insert into seats(seat_id, room_id) values (6, 1);
+insert into seats(seat_id, room_id) values (7, 1);
+insert into seats(seat_id, room_id) values (8, 1);
+insert into seats(seat_id, room_id) values (9, 1);
+insert into seats(seat_id, room_id) values (10, 1);
+insert into seats(seat_id, room_id) values (11, 2);
+insert into seats(seat_id, room_id) values (12, 2);
+insert into seats(seat_id, room_id) values (13, 2);
+insert into seats(seat_id, room_id) values (14, 2);
+insert into seats(seat_id, room_id) values (15, 2);
+insert into seats(seat_id, room_id) values (16, 2);
+insert into seats(seat_id, room_id) values (17, 2);
+insert into seats(seat_id, room_id) values (18, 2);
+insert into seats(seat_id, room_id) values (19, 2);
+insert into seats(seat_id, room_id) values (20, 2);
+insert into seats(seat_id, room_id) values (21, 3);
+insert into seats(seat_id, room_id) values (22, 3);
+insert into seats(seat_id, room_id) values (23, 3);
+insert into seats(seat_id, room_id) values (24, 3);
+insert into seats(seat_id, room_id) values (25, 3);
+insert into seats(seat_id, room_id) values (26, 3);
+insert into seats(seat_id, room_id) values (27, 3);
+insert into seats(seat_id, room_id) values (28, 3);
+insert into seats(seat_id, room_id) values (29, 3);
+insert into seats(seat_id, room_id) values (30, 3);
