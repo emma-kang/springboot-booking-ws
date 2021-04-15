dbc0255c395d19a40edaf37ef91a59ccc789d537
Emma Kang <yuseon1216@gmail.com>
2021-04-15 00:32:31 -0400

    Update SeatsDAO.java

--- src/main/java/com/ekang/studyroom/dao/SeatsDAO.java
+++ src/main/java/com/ekang/studyroom/dao/SeatsDAO.java
@@ -2,7 +2,6 @@ package com.ekang.studyroom.dao;
 
 import com.ekang.studyroom.dto.SeatsDTO;
 import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,9 +9,9 @@ import java.util.List;
 @Repository
 public class SeatsDAO {
     protected static final String NAMESPACE = "com.ekang.studyroom.mapper.SeatsMapper.";
-
-    @Autowired
     private SqlSession sqlSession;
+
+    public SeatsDAO(SqlSession sqlSession) { this.sqlSession = sqlSession; }
 
     public List<SeatsDTO> getAllSeats() {
         return sqlSession.selectList(NAMESPACE + "getAllSeats");
