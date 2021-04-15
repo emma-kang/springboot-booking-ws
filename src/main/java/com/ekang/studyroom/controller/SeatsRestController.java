2df127e1fd35f57e76df3a0e464201ff57a7f93b
Emma Kang <yuseon1216@gmail.com>
2021-04-15 00:31:58 -0400

    Update SeatsRestController.java

--- src/main/java/com/ekang/studyroom/controller/SeatsRestController.java
+++ src/main/java/com/ekang/studyroom/controller/SeatsRestController.java
@@ -4,7 +4,6 @@ import com.ekang.studyroom.dao.SeatsDAO;
 import com.ekang.studyroom.dto.SeatsDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
